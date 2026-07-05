082525d3097bea95b7be36576ef4d0e8fb0ad1ab
Bynntangg <bintang@gmail.com>
2026-07-05 09:50:52 +0700

    neww

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build'  // Biarkan ini karena Laravel sudah mengatur ke public/build
+    }
 });
