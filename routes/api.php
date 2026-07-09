dc54e6ac7e4498f812d7127c8ebaee9c74eff1f1
Bynntangg <bintang@gmail.com>
2026-07-09 23:08:04 +0700

    lkskd

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/csrf-token', function () {
+    return response()->json([
+        'csrf_token' => csrf_token()
+    ]);
+});
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
